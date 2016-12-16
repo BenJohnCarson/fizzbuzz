e7f127e0b182a9bdabc648496544d00dd86a6484
Benjamin Carson <benjohncarson@gmail.com>
2016-12-16 15:51:28 +0000

    returns buzz for multiples of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,10 +9,18 @@ describe "fizzbuzz" do
         expect(fizzbuzz(2)).to eq 2
     end
     
-    it "returns 3 if number is a multiple of 3" do
+    it "returns fizz if number is a multiple of 3" do
         (1..20).each do |n|
             if n % 3 == 0
                 expect(fizzbuzz(n)).to eq "fizz"
+            end
+        end
+    end
+    
+    it "returns buzz when passed multiple of 5" do
+        (1..10).each do |n|
+            if n % 5 == 0
+                expect(fizzbuzz(n)).to eq "buzz"
             end
         end
     end
