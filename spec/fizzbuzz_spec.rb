a4c15c103c9c793fc1d20911bda8f5ea8e0d42dc
Benjamin Carson <benjohncarson@gmail.com>
2016-12-16 15:39:51 +0000

    returns fizz for multiples of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,16 @@ describe "fizzbuzz" do
     it "returns fizz when passed 3" do
         expect(fizzbuzz(3)).to eq "fizz"
     end
+    
+    it "returns input when not 3" do
+        expect(fizzbuzz(2)).to eq 2
+    end
+    
+    it "returns 3 if number is a multiple of 3" do
+        (1..20).each do |n|
+            if n % 3 == 0
+                expect(fizzbuzz(n)).to eq "fizz"
+            end
+        end
+    end
 end
