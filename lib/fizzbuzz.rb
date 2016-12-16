17355e03634dffe422d306f0eaef455188ed6690
Benjamin Carson <benjohncarson@gmail.com>
2016-12-16 16:13:57 +0000

    code complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,6 @@
 def fizzbuzz(number)
+    return "fizzbuzz" if (number % 5 == 0) && (number % 3 == 0) 
+    
     if number % 3 == 0
         "fizz"
     elsif number % 5 == 0
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,13 +5,17 @@ describe "fizzbuzz" do
         expect(fizzbuzz(3)).to eq "fizz"
     end
     
-    it "returns input when not 3" do
-        expect(fizzbuzz(2)).to eq 2
+    it "returns input when not multiple of 3 or 5" do
+        (1..100).each do |n|
+            if (n % 3 != 0) && (n % 5 != 0)
+                expect(fizzbuzz(n)).to eq n
+            end
+        end
     end
     
-    it "returns fizz if number is a multiple of 3" do
+    it "returns fizz if number is a multiple of 3 and not of 5" do
         (1..20).each do |n|
-            if n % 3 == 0
+            if (n % 3 == 0) && (n % 5 != 0)
                 expect(fizzbuzz(n)).to eq "fizz"
             end
         end
@@ -21,6 +25,14 @@ describe "fizzbuzz" do
         (1..10).each do |n|
             if n % 5 == 0
                 expect(fizzbuzz(n)).to eq "buzz"
+            end
+        end
+    end
+    
+    it "returns fizzbuzz when passed multiple of 3 and 5" do
+        (1..20).each do |n|
+            if (n % 5 == 0) && (n % 3 == 0)
+                expect(fizzbuzz(n)).to eq "fizzbuzz"
             end
         end
     end
